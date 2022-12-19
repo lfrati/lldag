0c934694b7a9d721335020e99dbdf94eedf6cb2b
lfrati <fratilapo@gmail.com>
2022-12-19 23:26:39 +0100

    Time python version, slower but pretty fast >_>

--- extra/forward.py
+++ extra/forward.py
@@ -1,13 +1,6 @@
 import math
 from random import random
-
-"""
-I want to implement a llvmlite implementation of some neat-like graph.
-Pros:
-- Fast
-Cons:
-- ???
-"""
+from time import monotonic
 
 
 class Input:
@@ -19,7 +12,6 @@ class Input:
     def query(self):
         if not self.value:
             self.value = self.const
-            print(f"   {self.label} = {self.value}")
 
     def __repr__(self):
         return f"Input : {self.value}"
@@ -43,10 +35,7 @@ class Node:
 
             values = [w * input.value for w, input in zip(self.weights, self.inputs)]
             pre = sum(values)
-            print(f"{self.label}: values = sum({values})")
             self.value = self.op(pre)
-            print(f"{self.label}: pre = {self.op}({pre})")
-            print(f"{self.label}: {self.label} = {self.value}")
         return self.value
 
     def __repr__(self):
@@ -54,7 +43,6 @@ class Node:
 
 
 inputs = [Input(1, "X0"), Input(-1, "X1"), Input(2, "X2")]
-print(inputs)
 
 
 class Op:
@@ -72,16 +60,15 @@ class Sin(Op):
         return math.sin(x)
 
 
-A = Node(ReLU(), [inputs[0], inputs[2]], "A")
-B = Node(ReLU(), [inputs[1], inputs[2]], "B")
-C = Node(ReLU(), [inputs[1], inputs[2]], "C")
-D = Node(ReLU(), [A, B, C], "D")
-print(A)
-print(B)
-print(C)
-print(D)
-
-print(D.query())
+# A = Node(ReLU(), [inputs[0], inputs[2]], "A")
+# B = Node(ReLU(), [inputs[1], inputs[2]], "B")
+# C = Node(ReLU(), [inputs[1], inputs[2]], "C")
+# D = Node(ReLU(), [A, B, C], "D")
+# print(A)
+# print(B)
+# print(C)
+# print(D)
+# print(D.query())
 
 #%%
 
@@ -106,40 +93,45 @@ wiring = {
 weights = {
     key: [random() * 2 - 1 for _ in range(len(wiring[key]))] for key in wiring.keys()
 }
-
-
-def query(output, inputs, context=None):
-    if not context:
-        context = {}
-    _query(output, inputs, context)
-    return context
-
-
-def _query(node, inputs, context):
-    if type(node) == int and not node in context:
-        context[node] = inputs[node]
-        print(f"   X{node}={context[node]}")
-        return
-
-    # only evaluate a common subtree once
-    if not node in context:
-        inps, op = wiring[node]
-        for inp in inps:
-            _query(inp, inputs, context)
-
-        values = [w * context[pred] for w, pred in zip(weights[node], inps)]
-        pre = sum(values)
-        context[node] = op(pre)
-        print(f"{node}: pre = sum({values})")
-        print(f"{node}: {node} = {op}(pre) --> {context[node]}")
-
-    return context[node]
-
-
 inputs = [1, -1, 2]
 outputs = ["D", "E"]
-context = {}
-for out in outputs:
-    context = query(out, inputs, context)
 
-print(f"   return [{','.join(outputs)}]")
+
+def run(inputs, outputs, wiring, weights):
+    context = {}
+
+    def query(output, inputs, context=None):
+        if not context:
+            context = {}
+        _query(output, inputs, context)
+        return context
+
+    def _query(node, inputs, context):
+        if type(node) == int and not node in context:
+            context[node] = inputs[node]
+            return
+
+        # only evaluate a common subtree once
+        if not node in context:
+            inps, op = wiring[node]
+            for inp in inps:
+                _query(inp, inputs, context)
+
+            values = [w * context[pred] for w, pred in zip(weights[node], inps)]
+            pre = sum(values)
+            context[node] = op(pre)
+
+        return context[node]
+
+    for out in outputs:
+        context = query(out, inputs, context)
+
+    return [context[out] for out in outputs]
+
+
+CNT = 1_000
+start = monotonic()
+for _ in range(CNT):
+    _ = run(inputs, outputs, wiring, weights)
+end = monotonic()
+print(f"{(end - start)/CNT:.9f}")
