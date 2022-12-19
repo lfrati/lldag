256d09b00a836ff31e242d497178abc2b1ea65b9
lfrati <fratilapo@gmail.com>
2022-12-19 23:25:54 +0100

    Create llvm/cfunc and run.

--- graph.py
+++ graph.py
@@ -1,4 +1,5 @@
-from ctypes import POINTER, c_float
+from ctypes import CFUNCTYPE, POINTER, c_float, c_int
+from time import monotonic
 
 from llvmlite import ir
 import numpy as np
@@ -6,12 +7,20 @@ from rich import print as pprint
 
 from llvm import LLVM
 
+CNT = 1_000  # runs to time
 VALS = -1  # Values counter
 ACTS = -1  # Values counter
 NEUS = -1  # Neurons counter
 CONSTANTS = {}
 
 c_float_p = POINTER(c_float)
+
+zeroi = ir.Constant(ir.IntType(32), 0)
+zerof = ir.Constant(ir.FloatType(), 0)
+
+
+def index(i):
+    return ir.Constant(ir.IntType(32), i)
 
 
 class Op:
@@ -23,12 +32,10 @@ class ReLU(Op):
     def __call__(self, pre):
         global ACTS
         ACTS += 1
-        label = f"a{ACTS}"
-        cmp = builder.fcmp_ordered(
-            "<=", ir.Constant(ir.FloatType(), 0), pre.label, flags=("fast",)
-        )
-        label = builder.select(cmp, pre.label, ir.Constant(ir.FloatType(), 0))
-        return label
+        data = f"a{ACTS}"
+        cmp = builder.fcmp_ordered("<=", zerof, pre.data, flags=("fast",))
+        data = builder.select(cmp, pre.data, zerof)
+        return data
 
 
 class Nop(Op):
@@ -37,28 +44,28 @@ class Nop(Op):
 
 
 class Value:
-    def __init__(self, label):
-        self.label = label
+    def __init__(self, data):
+        self.data = data
 
     def query(self):
-        return self.label
+        return self.data
 
     def __repr__(self):
-        return self.label
+        return self.data
 
     def __mul__(self, other):
         global VALS
         VALS += 1
-        label = f"v{VALS}"
-        label = builder.fmul(self.label, other.label)
-        return Value(label)
+        data = f"v{VALS}"
+        data = builder.fmul(self.data, other.data)
+        return Value(data)
 
     def __add__(self, other):
         global VALS
         VALS += 1
-        label = f"v{VALS}"
-        label = builder.fadd(self.label, other.label)
-        return Value(label)
+        data = f"v{VALS}"
+        data = builder.fadd(self.data, other.data)
+        return Value(data)
 
     def __radd__(self, other):
         return self.__add__(other)
@@ -70,68 +77,55 @@ class Value:
 class Const(Value):
     def __init__(self, c):
         if c in CONSTANTS:
-            self.label = CONSTANTS[c]
+            self.data = CONSTANTS[c]
         else:
-            self.label = ir.Constant(ir.FloatType(), c)
-            CONSTANTS[c] = self.label
+            self.data = ir.Constant(ir.FloatType(), c)
+            CONSTANTS[c] = self.data
 
 
 class Input(Value):
     def __init__(self, off=0):
         self.off = off
-        self.label = f"inp_{off}"  # for builder
+        self.data = f"inp_{off}"  # for builder
 
     def query(self):
-        self.label = builder.load(
-            builder.gep(fargs[0], [ir.Constant(ir.IntType(32), self.off)])
-        )
-        return self.label
+        self.data = builder.load(builder.gep(fargs[0], [index(self.off)]))
+        return self.data
 
 
 class Weight(Value):
     def __init__(self, ix=0, off=0):
         self.ix = ix
         self.off = off
-        self.label = f"w{ix}_{off}"  # for builder
+        self.data = f"w{ix}_{off}"  # for builder
 
     def query(self):
-        self.label = builder.load(
-            builder.gep(fargs[self.ix], [ir.Constant(ir.IntType(32), self.off)])
-        )
-        return self.label
+        self.data = builder.load(builder.gep(fargs[self.ix], [index(self.off)]))
+        return self.data
 
 
 class Neuron(Value):
-    def __init__(self, op=None, name=None):
-        global NEUS
-        NEUS += 1
-        self.ix = NEUS
-        if op:
-            self.op = op
-        else:
-            self.op = Nop()
+    def __init__(self, name):
+        self.ix = neuron2idx[name]
+        self.op = Nop()
         self.inputs = []
         self.weights = []
-        self.label = None
+        self.data = None
         self.name = name
 
     def query(self):
-        if not self.label:
+        if not self.data:
             pre = Const(0)
             for _, (inp, w) in enumerate(zip(self.inputs, self.weights)):
                 inp.query()
                 w.query()
                 pre = pre + (inp * w)
-            self.label = self.op(pre)
-        else:
-            pprint(f"#[green] Neuron {self.name} CACHED")
-        return self.label
+            self.data = self.op(pre)
+        return self.data
 
     def __repr__(self):
-        return f"{self.name} : {self.op}({[el.label for el in self.inputs]})"
+        return f"{self.name} : {self.op}({[el.data for el in self.inputs]})"
 
-
-# print(D.query())
 
 ops = {
     "A": ReLU(),
@@ -140,7 +134,9 @@ ops = {
     "D": ReLU(),
     "E": ReLU(),
 }
+inputs = np.array([1.0, -1.0, 2.0], dtype=np.float32)
 outputs = ["D", "E"]
+results = np.zeros(len(outputs), dtype=np.float32)
 wiring = {
     "A": [0, 2],
     "B": [1, 2],
@@ -169,16 +165,14 @@ llvm_manager = LLVM()
 neurons = build(wiring, ops)
 
 for neuron in neurons.values():
-    # for x in node.inputs:
     print(neuron)
 print("------------")
 
 
-void = ir.VoidType()
 float_ptr = ir.PointerType(ir.FloatType())
 # there must be a pointer for each neuron
-NARGS = 1 + len(wiring.keys())  # +1 for input
-fnty = ir.FunctionType(ir.FloatType(), (float_ptr for _ in range(NARGS)))
+NARGS = 2 + len(wiring.keys())  # +1 for input +1 for outputs
+fnty = ir.FunctionType(ir.IntType(32), (float_ptr for _ in range(NARGS)))
 # Create an empty module...
 module = ir.Module(name=__file__)
 # and declare a function named "fpadd" inside it
@@ -194,8 +188,11 @@ retvals = []
 for output in outputs:
     if output in neurons:
         retvals.append(neurons[output].query())
-print("TO RETURN")
-builder.ret(retvals[0])
+
+
+for i, retval in enumerate(retvals):
+    builder.store(retval, builder.gep(fargs[-1], [index(i)]))
+builder.ret(zeroi)
 
 print(">>> LLVM IR ================================")
 print(module)
@@ -213,8 +210,28 @@ print(">>> ASM ====================================")
 llvm_manager.show_asm(comp_mod)
 print("============================================\n")
 
-weights = [np.random.rand(len(edges)) for edges in wiring.values()]
+args = (
+    [inputs]
+    + [np.random.rand(len(edges)).astype(dtype=np.float32) for edges in wiring.values()]
+    + [results]
+)
+pprint(args)
 
 # prepare arrays to pass them as pointers
-# print([w.ctypes.data_as(c_float_p) for w in weights])
-# args = []
+args = [w.ctypes.data_as(c_float_p) for w in args]
+
+func_ptr = llvm_manager.get_fptr("grapher")
+
+# Run the function via ctypes
+c_float_p = POINTER(c_float)
+cfunc = CFUNCTYPE(c_int, *([c_float_p] * NARGS))(func_ptr)
+start = monotonic()
+for _ in range(CNT):
+    ret = cfunc(*args)
+end = monotonic()
+print(f"{(end - start)/CNT:.9f}")
+# rint("  retval =", ret)
+print(results)
+
+# print("expected =", (inputs * weights).sum())
+# assert ret == (inputs * weights).sum()
