8712cc59a9add2af4d5284243b3eef1cb931132f
lfrati <fratilapo@gmail.com>
2022-12-20 10:38:31 +0100

    Increase max number of nodes in dag builder

--- builder.py
+++ builder.py
@@ -1,12 +1,31 @@
 import numpy as np
 from string import ascii_uppercase
+from itertools import product
+from typing import Iterator
 
 
-def make_dag(n_inputs=8, min_edges=3, max_edges=6, seed=1337):
+def make_names(N: int) -> Iterator[str]:
+    """
+    Generate N unique strings, lexicographically ordered
+    """
+    l = N // len(ascii_uppercase) + 1
+    for i, line in enumerate(product(ascii_uppercase, repeat=l)):
+        if i >= N:
+            return
+        yield "".join(line)
+
+
+def make_dag(
+    n_nodes: int = 32,
+    n_inputs: int = 8,
+    min_edges: int = 3,
+    max_edges: int = 6,
+    seed: int = 1337,
+) -> dict[str, list[str]]:
     np.random.seed(seed)
     pool = [str(i) for i in range(n_inputs)]
     wiring = {}
-    for node in ascii_uppercase:
+    for node in make_names(n_nodes):
         edges = np.random.randint(min_edges, max_edges)
         wiring[node] = np.random.choice(pool, size=(edges), replace=False).tolist()
         pool.append(node)
@@ -15,9 +34,11 @@ def make_dag(n_inputs=8, min_edges=3, max_edges=6, seed=1337):
 
 
 def main():
-    wiring = make_dag(n_inputs=16, min_edges=6, max_edges=10)
+    wiring = make_dag(n_nodes=32, n_inputs=16, min_edges=4, max_edges=5)
+    print("{")
     for neuron, edges in wiring.items():
         print(f'"{neuron}": { [int(e) if e.isdigit() else e for e in edges]},')
+    print("}")
 
 
 if __name__ == "__main__":
