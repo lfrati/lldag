31448b55533dff1e4d294d5ee83d22e15c7b7681
lfrati <fratilapo@gmail.com>
2023-01-29 00:44:49 -0500

    Fix Vectors in looping version. Closes #5

--- loop_llvmlite.py
+++ loop_llvmlite.py
@@ -5,11 +5,14 @@ from llvmlite import ir
 
 # import llvmlite.binding as llvm
 import numpy as np
+import os
 
 from llvm import LLVM
 
-N = 64
+N = 4
 NLOOPS = 2
+
+FMA = bool(os.getenv("FMA", 0))
 
 
 def i32(i):
@@ -20,60 +23,41 @@ def f32(i):
     return ir.Constant(ir.FloatType(), i)
 
 
-def make_single_loop():
-    # Create some useful types
-    float_ptr = ir.PointerType(ir.FloatType())
-    # void = ir.VoidType()
-    fnty = ir.FunctionType(ir.FloatType(), (float_ptr, float_ptr, ir.IntType(32)))
+def neuron(builder, inputs, weights, n, outputs, i, prev, curr_body, curr_exit, next):
 
-    # Create an empty module...
-    module = ir.Module(name=__file__)
-
-    func = ir.Function(module, fnty, name="node")
-
-    # the entry block allows phi to distinguish the first iteration
-    bb_entry = func.append_basic_block(name="entry")
-    bb_loop = func.append_basic_block(name="loop")
-    bb_exit = func.append_basic_block(name="exit")
-
-    builder = ir.IRBuilder()
-
-    # with multiple block I set the builder position like a cursor
-    builder.position_at_end(bb_entry)
-    builder.branch(bb_loop)
-    builder.position_at_end(bb_loop)
+    builder.position_at_end(curr_body)
 
     # prepare looping: use phi to init indeces and accumulator on entry into block
     index = builder.phi(ir.IntType(32))
     # phi nodes use .add_incoming to specigy the values to take depending on the provenance.
-    index.add_incoming(
-        ir.Constant(index.type, 0), bb_entry
-    )  # set to 0 if coming from bb_entry
-    accum = builder.phi(ir.FloatType())
-    accum.add_incoming(ir.Constant(accum.type, 0), bb_entry)
+    # set to 0 if coming from bb_entry
+    index.add_incoming(ir.Constant(index.type, 0), prev)
+    accum = builder.phi(ir.FloatType(), name="accum")
+    accum.add_incoming(ir.Constant(accum.type, 0), prev)
 
-    xs, ws, n = func.args
-
-    xs_ptr = builder.gep(xs, [index])
-    ws_ptr = builder.gep(ws, [index])
+    xs_ptr = builder.gep(inputs, [index])
+    ws_ptr = builder.gep(weights, [index])
     input = builder.load(xs_ptr)
     weight = builder.load(ws_ptr)
 
-    value = builder.fmul(input, weight)
+    if FMA:
+        added = builder.fma(input, weight, accum)
+    else:
+        value = builder.fmul(input, weight)
+        added = builder.fadd(accum, value)
 
-    added = builder.fadd(accum, value)
-    accum.add_incoming(added, bb_loop)
+    accum.add_incoming(added, curr_body)
 
     indexp1 = builder.add(index, ir.Constant(index.type, 1))
-    index.add_incoming(indexp1, bb_loop)  # increment by one if coming from bb_loop
+    index.add_incoming(indexp1, curr_body)  # increment by one if coming from bb_loop
 
     cond = builder.icmp_unsigned("<", indexp1, n)
-    builder.cbranch(cond, bb_loop, bb_exit)
+    builder.cbranch(cond, curr_body, curr_exit)
 
-    builder.position_at_end(bb_exit)
-    builder.ret(added)
-
-    return module
+    builder.position_at_end(curr_exit)
+    outputs = builder.insert_element(outputs, added, i32(i))
+    builder.branch(next)
+    return outputs
 
 
 def make_multi_loop():
@@ -87,67 +71,35 @@ def make_multi_loop():
     module = ir.Module(name=__file__)
 
     func = ir.Function(module, fnty, name="node")
+    vec = ir.Constant(ir.VectorType(ir.FloatType(), NLOOPS), None)
+    print(vec)
+    print(vec.type)
 
     # the entry block allows phi to distinguish the first iteration
     bb_entry = func.append_basic_block(name="entry")
+    bb_loops = [func.append_basic_block(name=f"loop_body_{i}") for i in range(NLOOPS)]
+    bb_exits = [func.append_basic_block(name=f"loop_exit_{i}") for i in range(NLOOPS)]
     bb_exit = func.append_basic_block(name="exit")
-    bb_loops = [func.append_basic_block(name=f"loop_{i}") for i in range(NLOOPS)]
-    blocks = [bb_entry] + bb_loops + [bb_exit]
 
     builder = ir.IRBuilder()
 
     # with multiple block I set the builder position like a cursor
     builder.position_at_end(bb_entry)
-    results = builder.alloca(ir.FloatType(), size=NLOOPS, name="results")
+
     builder.branch(bb_loops[0])
 
-    # # results = builder.alloca(ir.FloatType(), size=NLOOPS, name="results")
-    # results_ptr = ir.GlobalVariable(
-    #     module=module,
-    #     typ=ir.ArrayType(element=ir.FloatType(), count=NLOOPS),
-    #     name="results",
-    # )
+    xs, ws, n = func.args
 
-    for i, (prev, curr, next) in enumerate(zip(blocks, blocks[1:], blocks[2:])):
-        builder.position_at_end(curr)
+    for i in range(NLOOPS):
+        prev = bb_entry if i == 0 else bb_exits[i - 1]
+        curr_body = bb_loops[i]
+        curr_exit = bb_exits[i]
+        next = bb_loops[i + 1] if i < len(bb_loops) - 1 else bb_exit
 
-        # prepare looping: use phi to init indeces and accumulator on entry into block
-        index = builder.phi(ir.IntType(32))
-        # phi nodes use .add_incoming to specigy the values to take depending on the provenance.
-        index.add_incoming(
-            ir.Constant(index.type, 0), prev
-        )  # set to 0 if coming from bb_entry
-        accum = builder.phi(ir.FloatType(),name="accum")
-        accum.add_incoming(ir.Constant(accum.type, 0), prev)
-
-        xs, ws, n = func.args
-
-        xs_ptr = builder.gep(xs, [index])
-        ws_ptr = builder.gep(ws, [index])
-        input = builder.load(xs_ptr)
-        weight = builder.load(ws_ptr)
-
-        # value = builder.fmul(input, weight)
-        # added = builder.fadd(accum, value)
-
-        added = builder.fma(input, weight, accum)
-
-        # val = builder.store(added, ptr)
-        # builder.insert_value(added, results_ptr, int32(0))
-
-        # builder.insert_value(added, all, int32(0))
-
-        accum.add_incoming(added, curr)
-
-        indexp1 = builder.add(index, ir.Constant(index.type, 1))
-        index.add_incoming(indexp1, curr)  # increment by one if coming from bb_loop
-
-        cond = builder.icmp_unsigned("<", indexp1, n)
-        builder.store(added, builder.gep(results, [i32(i)]))
-        builder.cbranch(cond, curr, next)
+        vec = neuron(builder, xs, ws, n, vec, i, prev, curr_body, curr_exit, next)
 
     builder.position_at_end(bb_exit)
-    val = reduce_sum(builder, results, NLOOPS)
+    val = reduce_sum(builder, vec, NLOOPS)
     builder.ret(val)
 
     return module
@@ -156,9 +108,9 @@ def make_multi_loop():
 def reduce_sum(builder, ptr, N):
     acc = None
     for i in range(N):
-        value = builder.load(builder.gep(ptr, [i32(i)]))
+        value = builder.extract_element(ptr, i32(i))
         if i == 0:
-            acc = builder.fadd(f32(0), value)
+            acc = value
         else:
             acc = builder.fadd(acc, value)
     return acc
@@ -166,7 +118,6 @@ def reduce_sum(builder, ptr, N):
 
 llvm_manager = LLVM()
 
-# module = make_single_loop()
 module = make_multi_loop()
 
 print(">>> LLVM IR ================================")
@@ -175,15 +126,15 @@ print("============================================\n")
 
 opt_module = llvm_manager.optimize_ir(module)
 
-# print(">>> OPTIMIZED ==============================")
-# print(opt_module)
-# print("============================================\n")
+print(">>> OPTIMIZED ==============================")
+print(opt_module)
+print("============================================\n")
 
 comp_mod = llvm_manager.compile_ir(opt_module)
 
-# print(">>> ASM ====================================")
-# llvm_manager.show_asm(comp_mod)
-# print("============================================\n")
+print(">>> ASM ====================================")
+llvm_manager.show_asm(comp_mod)
+print("============================================\n")
 
 # Look up the function pointer (a Python int)
 func_ptr = llvm_manager.get_fptr("node")
@@ -198,13 +149,14 @@ st = monotonic()
 inps = inputs.ctypes.data_as(c_float_p)
 ws = weights.ctypes.data_as(c_float_p)
 et = monotonic()
-print(f"CAST: {et - st: .8f}")
+print(f"CAST: {et - st: .8f}s")
 
+print("Running...")
 st = monotonic()
 ret = cfunc(inps, ws, N)
 expected = (inputs * weights).sum() * NLOOPS
 et = monotonic()
-print(f" RUN: {et - st: .8f}")
+print(f" RUN: {et - st: .8f}s")
 
 assert ret == expected, f"Unexpected retval: {ret=} != {expected=}"
 print("MATCH: OK")
--- unrolled_llvmlite.py
+++ unrolled_llvmlite.py
@@ -4,6 +4,7 @@ from time import monotonic
 
 from llvmlite import ir
 import numpy as np
+from rich import print as pprint
 
 from llvm import LLVM
 
@@ -12,36 +13,10 @@ def index(i):
     return ir.Constant(ir.IntType(32), i)
 
 
-def make_fun_arr(fma=False):
-    arr = ir.ArrayType(ir.FloatType(), N)
-    fnty = ir.FunctionType(ir.FloatType(), (arr, arr))
-
-    # Create an empty module...
-    module = ir.Module(name=__file__)
-
-    func = ir.Function(module, fnty, name="node")
-
-    block = func.append_basic_block(name="entry")
-    builder = ir.IRBuilder(block)
-    accum = ir.Constant(ir.FloatType(), 0)
-    xs, ws = func.args
-    for i in range(N):
-        x = builder.extract_value(xs, i)
-        w = builder.extract_value(ws, i)
-
-        if fma:
-            accum = builder.fma(x, w, accum)
-        else:
-            value = builder.fmul(x, w)
-            accum = builder.fadd(accum, value)
-
-    builder.ret(accum)
-
-    return module
+float_ptr = ir.PointerType(ir.FloatType())
 
 
-def make_fun_ptr(fma=False):
-    float_ptr = ir.PointerType(ir.FloatType())
+def make_fun(fma=False):
     fnty = ir.FunctionType(ir.FloatType(), (float_ptr, float_ptr))
 
     # Create an empty module...
@@ -71,28 +46,23 @@ def make_fun_ptr(fma=False):
 llvm_manager = LLVM()
 
 FMA = bool(int(os.getenv("FMA", "1")))
-ARR = bool(int(os.getenv("ARR", "1")))
 N = int(os.getenv("N", "8"))
 
 print(f"{FMA=}")
-print(f"{ARR=}")
 print(f"{N=}")
 
-if ARR:
-    module = make_fun_arr(FMA)
-else:
-    module = make_fun_ptr(FMA)
+module = make_fun(FMA)
 
 if N <= 32:
     print(">>> LLVM IR ================================")
-    print(module)
+    pprint(module)
     print("============================================\n")
 
 opt_module = llvm_manager.optimize_ir(module)
 
 if N <= 32:
     print(">>> OPTIMIZED ==============================")
-    print(opt_module)
+    pprint(opt_module)
     print("============================================\n")
 
 comp_mod = llvm_manager.compile_ir(module)
@@ -106,23 +76,18 @@ if N <= 32:
 func_ptr = llvm_manager.get_fptr("node")
 
 
-inputs = np.ones(N, dtype=np.float32)
-weights = np.ones(N, dtype=np.float32) * 2
+inputs = np.random.rand(N).astype(np.float32)
+weights = np.random.rand(N).astype(np.float32)
 
-if ARR:
-    c_float_arr = c_float * N
-    cfunc = CFUNCTYPE(c_float, c_float_arr, c_float_arr)(func_ptr)
-    inps, ws = (c_float_arr)(*inputs), (c_float_arr)(*weights)
-else:
-    c_float_p = POINTER(c_float)
-    cfunc = CFUNCTYPE(c_float, c_float_p, c_float_p)(func_ptr)
-    inps, ws = inputs.ctypes.data_as(c_float_p), weights.ctypes.data_as(c_float_p)
+c_float_p = POINTER(c_float)
+cfunc = CFUNCTYPE(c_float, c_float_p, c_float_p)(func_ptr)
+inps, ws = inputs.ctypes.data_as(c_float_p), weights.ctypes.data_as(c_float_p)
 
 st = monotonic()
 ret = cfunc(inps, ws)
 et = monotonic()
-expected = 2 * N
+expected = (inputs * weights).sum()
 print(f"Elapsed {et - st:.9f}")
 print("  retval =", ret)
 print("expected =", expected)
-assert ret == expected
+assert np.allclose(ret, expected)
