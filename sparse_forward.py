0805c0ad2117952d89fb7eda8be5c4d61f184546
lfrati <fratilapo@gmail.com>
2023-02-05 18:50:25 -0500

    Test sin, clean up experiments.

--- sparse_forward.py
+++ sparse_forward.py
@@ -58,34 +58,37 @@ def extract_subset(outputs, wiring):
     return subset, sensors, neurons
 
 
-NNODES = 16
-NINPS = 8
-NOUTS = 6
-MINED = 4
-MAXED = 6
-
-generator = np.random.default_rng(SEED)
-wiring = make_dag(
-    n_nodes=NNODES,
-    n_inputs=NINPS,
-    min_edges=MINED,
-    max_edges=MAXED,
-    seed=SEED,
-)
-outputs = list(wiring.keys())[-NOUTS:]
-
-all_weights = {
-    node: generator.normal(size=(len(wiring[node]))).astype(np.float32) * 0.1
-    for node in wiring
-}
+def op(x):
+    # return max(0, x)
+    return np.sin(x)
 
 
-def forward(inputs, neurons, outputs, subset):
+def forward(inputs, sensors, neurons, wiring, weights, outputs):
+    py_cache = {}
+    for sensor in sensors:
+        py_cache[sensor] = inputs[sensor]
+
+    for i, neuron in enumerate(neurons):
+        py_incoming = np.array([py_cache[val] for val in wiring[neuron]])
+        py_ws = weights[i]
+
+        py_z = op((py_ws * py_incoming).sum())
+
+        py_cache[neuron] = py_z
+
+    py_results = []
+    for i, out in enumerate(outputs):
+        py_results.append(py_cache[out])
+
+    return py_results
+
+
+def build(sensors, neurons, outputs, subset):
 
     float_ptr = ir.PointerType(ir.FloatType())
     fnty = ir.FunctionType(ir.VoidType(), [float_ptr] * (len(neurons) + 2))
     mod = ir.Module(name=__file__)
-    func = ir.Function(mod, fnty, name="node")
+    func = ir.Function(mod, fnty, name="forward")
     builder = ir.IRBuilder()
 
     inputs_ptr = func.args[0]
@@ -93,6 +96,7 @@ def forward(inputs, neurons, outputs, subset):
     results_ptr = func.args[-1]
 
     bb_entry = func.append_basic_block(name="entry")
+    # sin = mod.declare_intrinsic("llvm.sin", [ir.FloatType()])
     builder.position_at_end(bb_entry)
 
     cache = {}
@@ -107,10 +111,9 @@ def forward(inputs, neurons, outputs, subset):
         for j in range(len(subset[neuron])):
             w = builder.load(builder.gep(ws, [i32(j)]))
             x = incoming[j]
-            # val = builder.fmul(w, x)
-            # acc = builder.fadd(acc, val)
             acc = builder.fma(x, w, acc)
         z = relu(builder, acc)
+        # z = builder.call(sin, [acc])
 
         cache[neuron] = z
 
@@ -123,13 +126,13 @@ def forward(inputs, neurons, outputs, subset):
     mod = llvm_manager.optimize_ir(mod)
     mod = llvm_manager.compile_ir(mod)
     # Look up the function pointer (a Python int)
-    func_ptr = llvm_manager.get_fptr("node")
+    func_ptr = llvm_manager.get_fptr("forward")
 
     def ndptr(dtype, ndim, shape):
         return ndpointer(dtype=dtype, ndim=ndim, shape=shape)
 
     arr_ptrs = (
-        [ndptr(dtype=np.float32, ndim=1, shape=len(inputs))]
+        [ndptr(dtype=np.float32, ndim=1, shape=len(sensors))]
         + [ndptr(dtype=np.float32, ndim=1, shape=len(subset[node])) for node in neurons]
         + [ndptr(dtype=np.float32, ndim=1, shape=len(outputs))]
     )
@@ -142,14 +145,68 @@ def forward(inputs, neurons, outputs, subset):
     return cfunc
 
 
-subset, sensors, neurons = extract_subset(outputs, wiring)
+def main():
 
-cfunc = forward(sensors, neurons, outputs, subset)
+    NNODES = 256
+    NINPS = 32
+    NOUTS = 16
+    MINED = 4
+    MAXED = 16
 
-inputs = generator.random(NINPS).astype(np.float32)
-weights = [all_weights[neuron] for neuron in neurons]
-results = np.zeros(shape=len(outputs), dtype=np.float32)
+    generator = np.random.default_rng(SEED)
+    wiring = make_dag(
+        n_nodes=NNODES,
+        n_inputs=NINPS,
+        min_edges=MINED,
+        max_edges=MAXED,
+        seed=SEED,
+    )
+    outputs = list(wiring.keys())[-NOUTS:]
 
-cfunc(inputs, *weights, results)
+    all_weights = {
+        node: generator.normal(size=(len(wiring[node]))).astype(np.float32) * 0.1
+        for node in wiring
+    }
 
-print(" ".join([f"{val:>6.3f}" for val in results]))
+    subset, sensors, neurons = extract_subset(outputs, wiring)
+
+    cfunc = build(sensors, neurons, outputs, subset)
+
+    inputs = generator.random(NINPS).astype(np.float32)
+    weights = [all_weights[neuron] for neuron in neurons]
+    results = np.zeros(shape=len(outputs), dtype=np.float32)
+
+    tot_edges = sum([weight.size for weight in weights])
+    print(f"Network size: {tot_edges}")
+
+    CNT = 32
+
+    st = monotonic()
+    for _ in range(CNT):
+        cfunc(inputs, *weights, results)
+    et = monotonic()
+    ll_elapsed = (et - st) / CNT
+    print(f"llvm: {et - st:.6f}s ({tot_edges/ll_elapsed/1e6:.2f}M edges/second)")
+
+    py_results = []
+    st = monotonic()
+    for _ in range(CNT):
+        py_results = forward(inputs, sensors, neurons, wiring, weights, outputs)
+    et = monotonic()
+    py_elapsed = (et - st) / CNT
+    print(f"  py: {et - st:.6f}s ({tot_edges/py_elapsed/1e6:.2f}M edges/second)")
+
+    # print(" ".join([f"{val:>6.3f}" for val in results]))
+    assert np.allclose(
+        results, py_results
+    ), f"MISMATCH: {results.sum()} != {np.sum(py_results)}"
+    pprint("[yellow]MATCH: OK")
+
+    speedup = (ll_elapsed - py_elapsed) / py_elapsed
+
+    faster = "[green]faster" if speedup < 0 else "[red]slower"
+    pprint(f"LLVM vs PYTHON: llvm is {speedup * 100:.1f}% {faster}")
+
+
+if __name__ == "__main__":
+    main()
